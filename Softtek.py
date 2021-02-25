249cdd53b53b50eab933a6e19aba35209cfc61d7
hidden <64370910+hiddenpayload@users.noreply.github.com>
2021-02-25 17:04:05 -0600

    Update Softtek.py

--- Softtek.py
+++ Softtek.py
@@ -4,6 +4,8 @@ import time
 import os
 import logging
 
+#SORRY FOR ALL THE COMMENTS, MY LEADER WANTED ME TO ADD THEM AND I THINK IT WAS A GOOD IDEA TO LEAVE IT LIKE THIS FOR YOU TO SEE :)
+
 # Creating a variable of the current date
 ifiledate = datetime.today()
 # Creating a variable of the current date with 2 days less
@@ -19,17 +21,17 @@ logging.basicConfig(format=FORMAT)
 # Configuring GoogleChrome to be headless and creating a variable with the download folder
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
-download_path = r'C:\Users\rois.gr\OneDrive - Procter and Gamble\Documents\POS MEX\SOTDataQuality\Al Super\2020\12'
+download_path = r'C:\Users\PATH'
 # Creating a variable with the chromerdrive.exe so the script can use it
 # Changing the configuration of the browser
 # Changing the downloads folder to the folder below
-browser = webdriver.Chrome(executable_path=r'C:\Users\rois.gr\PycharmProjects\BrowserAut\chromedriver.exe', options=options)
+browser = webdriver.Chrome(executable_path=r'C:\Users\PATH\chromedriver.exe', options=options)
 browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
 params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_path}}
 command_result = browser.execute("send_command", params)
 # Open Chrome and go to the portal
 logging.warning('Opening the browser, going to the portal and closing a pop up...')
-browser.get('http://proveedores.alsuper.com/proveedores/')
+browser.get('http://webpage.com/directory/')
 
 #######
 #LOGIN#
@@ -41,8 +43,8 @@ username = browser.find_element_by_xpath('//*[@id="T1"]')
 password = browser.find_element_by_xpath('//*[@id="T2"]')
 # Filling the username and password fields with the current credentials
 logging.warning('Login in...')
-username.send_keys('16768')
-password.send_keys('PPACAC')
+username.send_keys('user')
+password.send_keys('12345')
 # Click on "Ingresar"
 browser.find_element_by_xpath('/html/body/div[1]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/table/tbody/tr[1]/td[1]/form/table/tbody/tr[6]/td/input').click()
 # Click on "He leido y acepto el aviso"
@@ -66,7 +68,7 @@ time.sleep(30)
 logging.warning('File download successful!')
 # Renaming de file
 logging.warning('Renaming file...')
-os.rename(r'C:\Users\rois.gr\OneDrive - Procter and Gamble\Documents\POS MEX\SOTDataQuality\Al Super\2020\12\ficheroExcel.xls', r'C:\Users\rois.gr\OneDrive - Procter and Gamble\Documents\POS MEX\SOTDataQuality\Al Super\2020\12\I_R3778_D_' + ifiledate.date().strftime('%Y%m%d') + ".xls")
+os.rename(r'C:\Users\PATHWITHCURRENTFILENAME', r'C:\Users\PATHWITHNEWFILENAME' + ifiledate.date().strftime('%Y%m%d') + ".xls")
 
 #########
 ##SALES##
@@ -95,7 +97,7 @@ time.sleep(15)
 logging.warning('File download successful!')
 # Renaming de file
 logging.warning('Renaming file...')
-os.rename(r'C:\Users\rois.gr\OneDrive - Procter and Gamble\Documents\POS MEX\SOTDataQuality\Al Super\2020\12\ficheroExcel.xls', r'C:\Users\rois.gr\OneDrive - Procter and Gamble\Documents\POS MEX\SOTDataQuality\Al Super\2020\12\V_R3778_D_' + sfiledate.date().strftime('%Y%m%d') + ".xls")
+os.rename(r'C:\Users\PATHWITHCURRENTFILENAME', r'C:\Users\PATHWITHNEWFILENAME' + sfiledate.date().strftime('%Y%m%d') + ".xls")
 
 logging.warning('Files were downloaded successfully!')
 logging.warning('Exiting...')
